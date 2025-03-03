4b85b24786f71343ca6876def93561c4257b9ff5
Leo Marxs <leonardohmarques2004@gmail.com>
2025-03-03 18:32:21 -0300

    add api

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function encurtarUrl() {
     let url = document.getElementById("input-url").value;
-    if(!url) {
+    if (!url) {
         alert("Insira uma url para encurtar!");
         return;
+    }
+
+    //api key: f89f5a6f5b5e45089850428d3e9ce18d
+
+    let headers = {
+        "Content-Type": "application/json",
+        "apiKey": "f89f5a6f5b5e45089850428d3e9ce18d"
+    }
+
+    let linkRequest = {
+        destination: url,
+        domain: { fullName: "rebrand.ly" }
     }
 }
