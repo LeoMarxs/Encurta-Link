5c4114891afea8b8272e5017316ea9b62e8eae5b
Leo Marxs <leonardohmarques2004@gmail.com>
2025-03-03 18:43:22 -0300

    finish api

--- script.js
+++ script.js
@@ -16,4 +16,16 @@ function encurtarUrl() {
         destination: url,
         domain: { fullName: "rebrand.ly" }
     }
+
+    fetch("https://api.rebrandly.com/v1/links", {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(linkRequest)
+    })
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            let inputUrl = document.getElementById("input-url");
+            inputUrl.value = json.shortUrl; 
+        });
 }
